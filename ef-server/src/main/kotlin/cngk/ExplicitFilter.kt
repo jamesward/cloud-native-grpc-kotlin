762209f15b954c7dd4869bb9602be05319439f4a
Ryan Knight <ryan@grandcloud.com>
2022-06-09 16:41:01 -0600

    adding bomb server status

--- ef-server/src/main/kotlin/cngk/ExplicitFilter.kt
+++ ef-server/src/main/kotlin/cngk/ExplicitFilter.kt
@@ -14,57 +14,28 @@ class ExplicitFilter : ExplicitFilterGrpcKt.ExplicitFilterCoroutineImplBase() {
             if (badWords.find { it == word.lowercase() } != null) {
                 word.map { "*" }.joinToString("")
             }
-            else if (word.startsWith("block:")) {
-                try {
-                    val input = word.subSequence(6,word.length).toString().toLong()
-                    val out = intenseFunc(input)
-                    out
-                } catch (nfe: NumberFormatException) {
-                    "Not a valid double"
-                }
-
+            else if (word.startsWith("bomb")) {
+                HealthCheck.hc_status = HealthCheckResponse.ServingStatus.NOT_SERVING
             }
             else {
                 word
             }
         }
 
-
-
         return text { body = filtered.joinToString(" ") }
     }
 
-    fun intenseFunc(input:Long):Long {
-        var result = 0L
-
-        val superIntense = input%2 == 0L
-        println("Intense starting with super intense set to: $superIntense")
-
-        for (indx in input downTo 0 step 1) {
-            val next = indx.toDouble().pow(27)
-            if ( superIntense) {
-                result += Math.atan(Math.tan(Math.atan(Math.tan(Math.atan(Math.tan(next)))))).roundToLong()
-            }
-            else {
-                result += Math.atan(Math.tan(Math.atan(next))).roundToLong()
-            }
-
-            if (indx % 1000000 == 0L) {
-                println("Still calculating - current result $result")
-            }
-
-        }
-
-        println("Intense finished")
-        return result;
-    }
 }
 
 class HealthCheck : HealthGrpcKt.HealthCoroutineImplBase() {
     override suspend fun check(request: HealthCheckRequest): HealthCheckResponse {
         return healthCheckResponse {
-            status = HealthCheckResponse.ServingStatus.SERVING
+            status = hc_status
         }
+    }
+
+    companion object {
+        var hc_status = HealthCheckResponse.ServingStatus.SERVING
     }
 }
 
