a371b5a6c499a7ede7266c200e8d55d058385a11
Ryan Knight <ryan@grandcloud.com>
2022-06-09 15:47:39 -0600

    adding cpu intense calc

--- ef-server/src/main/kotlin/cngk/ExplicitFilter.kt
+++ ef-server/src/main/kotlin/cngk/ExplicitFilter.kt
@@ -1,7 +1,8 @@
 package cngk
 
 import io.grpc.ServerBuilder
-import io.grpc.health.v1.*
+import kotlin.math.pow
+import kotlin.math.roundToLong
 
 class ExplicitFilter : ExplicitFilterGrpcKt.ExplicitFilterCoroutineImplBase() {
 
@@ -12,20 +13,49 @@ class ExplicitFilter : ExplicitFilterGrpcKt.ExplicitFilterCoroutineImplBase() {
             if (badWords.find { it == word.lowercase() } != null) {
                 word.map { "*" }.joinToString("")
             }
+            else if (word.startsWith("block:")) {
+                try {
+                    val input = word.subSequence(6,word.length).toString().toLong()
+                    val out = intenseFunc(input)
+                    out
+                } catch (nfe: NumberFormatException) {
+                    "Not a valid double"
+                }
+
+            }
             else {
                 word
             }
         }
 
+
+
         return text { body = filtered.joinToString(" ") }
     }
-}
 
-class HealthCheck : HealthGrpcKt.HealthCoroutineImplBase() {
-    override suspend fun check(request: HealthCheckRequest): HealthCheckResponse {
-        return healthCheckResponse {
-            status = HealthCheckResponse.ServingStatus.SERVING
+    fun intenseFunc(input:Long):Long {
+        var result = 0L
+
+        val superIntense = input%2 == 0L
+        println("Intense starting with super intense set to: $superIntense")
+
+        for (indx in input downTo 0 step 1) {
+            val next = indx.toDouble().pow(27)
+            if ( superIntense) {
+                result += Math.atan(Math.tan(Math.atan(Math.tan(Math.atan(Math.tan(next)))))).roundToLong()
+            }
+            else {
+                result += Math.atan(Math.tan(Math.atan(next))).roundToLong()
+            }
+
+            if (indx % 1000000 == 0L) {
+                println("Still calculating - current result $result")
+            }
+
         }
+
+        println("Intense finished")
+        return result;
     }
 }
 
@@ -37,7 +67,6 @@ fun main() {
     val server = ServerBuilder
             .forPort(port)
             .addService(ExplicitFilter())
-            .addService(HealthCheck())
             .build()
 
     server.start()
